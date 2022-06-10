fdf25603947e79cbc25303458b5d29e35545b587
Kjeld Schouten-Lebbing <kjeld@schouten-lebbing.nl>
2022-06-10 18:30:01 +0200

    plain html tryout

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -2,58 +2,38 @@ import React from 'react';
 import clsx from 'clsx';
 import styles from './styles.module.css';
 
-const FeatureList = [
-  {
-    title: 'Multi-cultureel',
-    src: require('@site/static/img/h1.jpg').default,
-    description: (
-      <>
-        Van oost-europees tot mediteraans, verschillende ethniciteiten geven kleur aan de straat
-      </>
-    ),
-  },
-  {
-    title: 'Nieuwbouw',
-    src: require('@site/static/img/h2.jpg').default,
-    description: (
-      <>
-        Prachtige nieuwe huizen, voorzien van de laatste moderne luxes
-      </>
-    ),
-  },
-  {
-    title: 'Gezellig',
-    src: require('@site/static/img/h2.jpg').default,
-    description: (
-      <>
-        Altijd wel iets te zien en iemand om mee te kletsen
-      </>
-    ),
-  },
-];
-
-function Feature({img, title, description}) {
-  return (
-    <div className={clsx('col col--4')}>
-      <div className="text--center">
-        <img src={src} className={styles.feature} role="img" />
-      </div>
-      <div className="text--center padding-horiz--md">
-        <h3>{title}</h3>
-        <p>{description}</p>
-      </div>
-    </div>
-  );
-}
-
 export default function HomepageFeatures() {
   return (
     <section className={styles.features}>
       <div className="container">
         <div className="row">
-          {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
-          ))}
+          <div className={clsx('col col--4')}>
+            <div className="text--center">
+              <img src="img/h1.jpg" className="featureImg" role="img" />
+            </div>
+            <div className="text--center padding-horiz--md">
+              <h3>Multi-cultureel</h3>
+              <p>Van oost-europees tot mediteraans, verschillende ethniciteiten geven kleur aan de straat</p>
+            </div>
+          </div>
+          <div className={clsx('col col--4')}>
+            <div className="text--center">
+              <img src="img/h2.jpg" className="featureImg" role="img" />
+            </div>
+            <div className="text--center padding-horiz--md">
+              <h3>Nieuwbouw</h3>
+              <p>Prachtige nieuwe huizen, voorzien van de laatste moderne luxes</p>
+            </div>
+          </div>
+          <div className={clsx('col col--4')}>
+            <div className="text--center">
+              <img src="img/h3.jpg" className="featureImg" role="img" />
+            </div>
+            <div className="text--center padding-horiz--md">
+              <h3>Gezellig</h3>
+              <p>Altijd wel iets te zien en iemand om mee te kletsen</p>
+            </div>
+          </div>
         </div>
       </div>
     </section>
